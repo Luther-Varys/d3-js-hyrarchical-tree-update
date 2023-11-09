17cd298cf0c26c73f26edfa77c5bcfa90c0300da
Carlo Bramini <braminicarlo@live.com>
2023-11-09 00:29:46 +0000

    Create layer for circle and links. Also Configures layout to horizontal

--- script.js
+++ script.js
@@ -6,6 +6,13 @@
 //---- How to and Bexier Curve links --------
 //https://stackoverflow.com/questions/55431818/change-d3-force-layout-link-style-to-match-d3-tree-look
 
+//---- How chat gpt says to move the cirles layer level above the links ----
+//I apologize for the confusion. The z-index property 
+//is not applicable to SVG elements. To achieve the 
+//effect of hiding links under shapes in a D3.js hierarchy 
+//tree, you can control the rendering order by carefully 
+//ordering the SVG elements within the <g> container. Here's how you can achieve this effect:
+
 
 // Define the data in a hierarchical format
 const data = {
@@ -23,8 +30,10 @@ const data = {
   };
   
   // Define dimensions of the SVG container
-  const width = 800;
-  const height = 400;
+  // const width = 800;
+  // const height = 400;
+  const width = 900;
+  const height = 900;  
   
   const svg = d3.select("#tree-container")
     .attr("width", width)
@@ -44,44 +53,53 @@ const data = {
   // Create links (edges) between nodes
   const links = root.links();
   
+  const linkContainer = svg.append("g");
+
+  // Create a container for the nodes
+  const nodeContainer = svg.append("g");
+
+
+
   // Create nodes (circles) for the tree
-  const nodes = svg
+  const nodes = nodeContainer
     .selectAll("circle")
     .data(root.descendants())
     .enter()
     .append("circle")
     .attr("cx", (d) => {
-      var resp = d.x;
+      var resp = d.y;
       return resp;
     })
     .attr("class", "wpnode")
-    .attr("cy", (d) => {return d.y;})
+    .attr("cy", (d) => {return d.x;})
     .attr("r", 10)
     .on("click", clicked)
-    .attr("data-id", (d)=>{return d.data.name;});
+    .attr("data-id", (d)=>{return d.data.name;})
+    // .style("z-index", 10); // Set a higher z-index for the circles
   
   // Create lines for the links
-  const linktest01 = d3.linkVertical()
-  .x(function(d) { return d.x; })
-  .y(function(d) { return d.y; });
+  const linktest01 = d3.linkHorizontal()
+  .x(function(d) { return d.y; })
+  .y(function(d) { return d.x; });
 
-  svg
+  linkContainer
     .selectAll("path")
     // .data(links)
     .data(links)
     .enter()
     .append("path")
+    .style("z-index", 111)
     .attr("d", linktest01);
   
   // Add labels to the nodes
-  svg
+  nodeContainer
     .selectAll("text")
     .data(root.descendants())
     .enter()
     .append("text")
     .text((d) => d.data.name)
-    .attr("x", (d) => d.x)
-    .attr("y", (d) => d.y - 20)
+    .attr("x", (d) => d.y)
+    .attr("y", (d) => d.x - 20)
     .attr("text-anchor", "middle");
   
 
@@ -105,31 +123,31 @@ setTimeout(()=>{
   //NODES
   nodes
     .data(root.descendants())
-    .attr("cx", (d) => d.x)
-    .attr("cy", (d) => d.y);  
+    .attr("cx", (d) => d.y)
+    .attr("cy", (d) => d.x);  
 
-  svg
+    nodeContainer
     .selectAll("circle")
     .data(root.descendants())
     .enter()
     .append("circle")
       .attr("cx", (d) => {
-        var resp = d.x;
+        var resp = d.y;
         return resp;
       })
       .attr("class", "wpnode")
-      .attr("cy", (d) => {return d.y;})
+      .attr("cy", (d) => {return d.x;})
       .attr("r", 10)
       .on("click", clicked)
       .attr("data-id", (d)=>{return d.data.name;});
 
   //LINKS
-  const linktest01 = d3.linkVertical()
-  .x(function(d) { return d.x; })
-  .y(function(d) { return d.y; });
+  const linktest01 = d3.linkHorizontal()
+  .x(function(d) { return d.y; })
+  .y(function(d) { return d.x; });
 
-  svg.selectAll("path").remove();
-  svg
+  linkContainer.selectAll("path").remove();
+  linkContainer
     .selectAll("path")
     .data(root.links())
     .enter()
@@ -140,15 +158,15 @@ setTimeout(()=>{
     .attr("d", linktest01);    
 
   //TEXT
-  svg.selectAll("text").remove();
-  svg
+  nodeContainer.selectAll("text").remove();
+  nodeContainer
     .selectAll("text")
     .data(root.descendants())
     .enter()
     .append("text")
       .text((d) => d.data.name)
-      .attr("x", (d) => d.x)
-      .attr("y", (d) => d.y - 20)
+      .attr("x", (d) => d.y)
+      .attr("y", (d) => d.x - 20)
       .attr("text-anchor", "middle");
 
 
@@ -172,31 +190,31 @@ setTimeout(()=>{
   //NODES
   nodes
     .data(root.descendants())
-    .attr("cx", (d) => d.x)
-    .attr("cy", (d) => d.y);  
+    .attr("cx", (d) => d.y)
+    .attr("cy", (d) => d.x);  
 
-    svg.selectAll("circle").remove();
-    svg
+    nodeContainer.selectAll("circle").remove();
+    nodeContainer
     .selectAll("circle")
     .data(root.descendants())
     .enter()
     .append("circle")
       .attr("cx", (d) => {
-        var resp = d.x;
+        var resp = d.y;
         return resp;
       })
       .attr("class", "wpnode")
       .on("click", clicked)
-      .attr("cy", (d) => {return d.y;})
+      .attr("cy", (d) => {return d.x;})
       .attr("r", 10)
       .attr("data-id", (d)=>{return d.data.name;});
 
-  const linktest01 = d3.linkVertical()
-  .x(function(d) { return d.x; })
-  .y(function(d) { return d.y; });
+  const linktest01 = d3.linkHorizontal()
+  .x(function(d) { return d.y; })
+  .y(function(d) { return d.x; });
   //LINKS
-  svg.selectAll("path").remove();
-  svg
+  linkContainer.selectAll("path").remove();
+  linkContainer
     .selectAll("path")
     .data(root.links())
     .enter()
@@ -207,15 +225,15 @@ setTimeout(()=>{
     .attr("d", linktest01);    
 
   //TEXT
-  svg.selectAll("text").remove();
-  svg
+  nodeContainer.selectAll("text").remove();
+  nodeContainer
     .selectAll("text")
     .data(root.descendants())
     .enter()
     .append("text")
       .text((d) => d.data.name)
-      .attr("x", (d) => d.x)
-      .attr("y", (d) => d.y - 20)
+      .attr("x", (d) => d.y)
+      .attr("y", (d) => d.x - 20) 
       .attr("text-anchor", "middle");
 
 
@@ -239,31 +257,32 @@ setTimeout(()=>{
   //NODES
   nodes
     .data(root.descendants())
-    .attr("cx", (d) => d.x)
-    .attr("cy", (d) => d.y);  
+    .attr("cx", (d) => d.y)
+    .attr("cy", (d) => d.x);  
 
-    svg
+    nodeContainer
     .selectAll("circle")
     .data(root.descendants())
     //.exit()
     .enter()
     .append("circle")
       .attr("cx", (d) => {
-        var resp = d.x;
+        var resp = d.y;
         return resp;
       })
-      .attr("cy", (d) => {return d.y;})
+      .attr("cy", (d) => {return d.x;})
       .attr("r", 10)
       .attr("class", "wpnode")
       .on("click", clicked)
+      // .style("z-index", 10)
       .attr("data-id", (d)=>{return d.data.name;});
 
   //LINKS
-  const linktest01 = d3.linkVertical()
-  .x(function(d) { return d.x; })
-  .y(function(d) { return d.y; });  
-  svg.selectAll("path").remove();
-  svg
+  const linktest01 = d3.linkHorizontal()
+  .x(function(d) { return d.y; })
+  .y(function(d) { return d.x; });  
+  linkContainer.selectAll("path").remove();
+  linkContainer
     .selectAll("path")
     .data(root.links())
     .enter()
@@ -274,15 +293,15 @@ setTimeout(()=>{
     .attr("d", linktest01);;    
 
   //TEXT
-  svg.selectAll("text").remove();
-  svg
+  nodeContainer.selectAll("text").remove();
+  nodeContainer
     .selectAll("text")
     .data(root.descendants())
     .enter()
     .append("text")
       .text((d) => d.data.name)
-      .attr("x", (d) => d.x)
-      .attr("y", (d) => d.y - 20)
+      .attr("x", (d) => d.y)
+      .attr("y", (d) => d.x - 20)
       .attr("text-anchor", "middle");  
 
 }, 6000);
